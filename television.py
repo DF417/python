53b7724036d4e470b7ce0da2ed5bc61013d6d6fe
DF417 <167568295+DF417@users.noreply.github.com>
2024-04-19 20:21:35 -0500

    main branch
    this is the creation of the main branch without docstrings and type hinting

---
+++ television.py
@@ -0,0 +1,71 @@
+class Television:
+    
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+    
+    def __init__(self):
+        
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
+    
+    def power(self):
+        
+        self.__muted = False
+        self.__status = not self.__status
+        
+    def mute(self):
+        
+        if self.__status == True:
+            self.__muted = not self.__muted
+
+    def channel_up(self):
+        
+        if self.__status == True:
+            if self.__channel >= Television.MAX_CHANNEL:
+                self.__channel = Television.MIN_CHANNEL
+            else:
+                self.__channel += 1
+
+    def channel_down(self):
+        
+        if self.__status == True:
+            if self.__channel <= Television.MIN_CHANNEL:
+                self.__channel = Television.MAX_CHANNEL
+            else:
+                self.__channel -= 1
+                
+    def volume_up(self):
+        
+
+        self.__muted = False
+        if self.__status == True:
+            if self.__volume >= Television.MAX_VOLUME:
+                self.__volume = Television.MAX_VOLUME
+            else:
+                self.__volume += 1
+
+    def volume_down(self):
+        
+        
+        self.__muted = False
+        if self.__status == True:
+            if self.__volume <= Television.MIN_VOLUME:
+                self.__volume = Television.MIN_VOLUME
+            else:
+                self.__volume -= 1
+
+    def __str__(self):
+        
+        power = self.__status
+        chan = self.__channel
+        
+        if self.__muted == True:
+            vol = 0
+        else:
+            vol = self.__volume
+
+        return f'Power = {power}, Channel = {chan}, Volume = {vol}'
