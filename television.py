649779f3e6c990e80ebe70db1c0f6edd4cba9f60
DF417 <167568295+DF417@users.noreply.github.com>
2024-04-21 21:07:10 -0500

    Update television.py

--- television.py
+++ television.py
@@ -60,12 +60,10 @@ class Television:
 
     def __str__(self):
         
-        power = self.__status
-        chan = self.__channel
-        
         if self.__muted == True:
-            vol = 0
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}'
         else:
-            vol = self.__volume
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
 
-        return f'Power = {power}, Channel = {chan}, Volume = {vol}'
+
+        
